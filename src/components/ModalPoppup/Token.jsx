0e5aacdbcdedac62c0dd247e40ab3c7be57fbfdb
anatolii-samurai <goddevil22@yandex.ru>
2024-05-24 15:46:19 +0400

    Сокрытие введенных данных в окно ввода токена 	modified:   src/components/ModalPoppup/Token.jsx

--- src/components/ModalPoppup/Token.jsx
+++ src/components/ModalPoppup/Token.jsx
@@ -1,40 +1,94 @@
 import { useCallback, useState } from 'react';
-import { Button,TextField, Typography } from '@mui/material';
+import { InputLabel,Input,IconButton,InputAdornment,Button } from '@mui/material';
 import Modal from './Modal';
+
+import { Visibility,VisibilityOff } from '@mui/icons-material';
+
 
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../context/Auth';
+import { token } from '../api/api';
+
+
 
 export default function Token(){
-    const [username, setUsername] = useState("");
+    // const [user, setUserToken] = useState("");
+    const [values, setValues] = useState({
+      password: "",
+      showPassword: false,
+  });
     const [modalActive,setModalActive] = useState(true)
     const { setUser } = useAuth();
     const navigate = useNavigate()
     const login = useCallback(
       (e) => {
         e.preventDefault();
-        setUser ({ username });
-        navigate("/");
+        
+          setUser ({ values });
+          navigate("/");
+        
       },
       // eslint-disable-next-line react-hooks/exhaustive-deps
-      [setUser, username]
+      [setUser, values]
     );
- 
+    
+    const handleClickShowPassword = () => {
+      setValues({
+          ...values,
+          showPassword: !values.showPassword,
+      });
+  };
+
+  const handleMouseDownPassword = (event) => {
+      event.preventDefault();
+  };
+
+  const handlePasswordChange = (prop) => (event) => {
+      setValues({
+          ...values,
+          [prop]: event.target.value,
+      });
+  };
    
     return(
         <>
         
         <Modal active ={modalActive} setModalActive={setModalActive}   >
           <form onSubmit={login}>
-          <Typography sx={{paddingBottom:'20px'}}>
+          <InputLabel sx={{paddingBottom:'20px'}} htmlFor="standard-adornment-password">
             Введите ключ!
-          </Typography>
-        <TextField required value={username} onChange={(e) => setUsername(e.target.value)}
-            size='small' id="outlined-basic" inputProps={{
-          type: "text",
-        }} label="Enter token" variant="outlined"   />
+          </InputLabel>
+          <Input
+                type={
+                    values.showPassword
+                        ? "text"
+                        : "password"
+                }
+                onChange={handlePasswordChange("password")}
+                defaultValue={token}
+                endAdornment={
+                    <InputAdornment position="end">
+                        <IconButton
+                            onClick={
+                                handleClickShowPassword
+                            }
+                            onMouseDown={
+                                handleMouseDownPassword
+                            }
+                        >
+                            {values.showPassword ? (
+                                <Visibility />
+                            ) : (
+                                <VisibilityOff  />
+                            )}
+                        </IconButton>
+                    </InputAdornment>
+                }
+            />
        
        <Button variant="outlined" type='submit' sx={{marginLeft:'20px'}}  onClick={()=>{setModalActive(false)}}>Отправить</Button>
+         
+         
           </form>
           
        
